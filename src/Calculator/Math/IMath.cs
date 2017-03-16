3b4ae4f551d81a1f3b1a978bd9dc1758081d9dc4
Vojtech Hertl <xhertl04@stud.fit.vutbr.cz>
2017-03-16 14:00:06 +0100

    #6 Added domains.

--- src/Calculator/Math/IMath.cs
+++ src/Calculator/Math/IMath.cs
@@ -30,13 +30,13 @@ namespace Disassembler
 		/// <returns>The result of the substraction.</returns>
 		double sub(double num1, double num2);
 
-		/// <summary>
-		/// Divides two numbers.
-		/// </summary>
-		/// <param name="num1">Divident.</param>
-		/// <param name="num2">Divisor.</param>
-		/// <returns>The result of the division.</returns>
-		double div(double num1, double num2);
+        /// <summary>
+        /// Divides two numbers.
+        /// </summary>
+        /// <param name="num1">Divident.</param>
+        /// <param name="num2">Divisor. num2 in R-{0}</param>
+        /// <returns>The result of the division.</returns>
+        double div(double num1, double num2);
 
 		/// <summary>
 		/// Multiplies two numbers.
@@ -49,31 +49,31 @@ namespace Disassembler
 		/// <summary>
 		/// Cumputes a factorial.
 		/// </summary>
-		/// <param name="num">Factoriated number.</param>
+		/// <param name="num">Factoriated number. num in N</param>
 		/// <returns>Factorial from the number.</returns>
 		long fact(long num);
 
 		/// <summary>
 		/// Exponentiation with general exponent.
 		/// </summary>
-		/// <param name="num1">Base.</param>
-		/// <param name="num2">Exponent.</param>
+		/// <param name="num1">Base. num1 in (0,inf)-{1} </param>
+		/// <param name="num2">Exponent. num2 in (0,inf)</param>
 		/// <returns>The power.</returns>
 		double exp(double num1, double num2);
 
 		/// <summary>
 		/// Root with general degree.
 		/// </summary>
-		/// <param name="num1">Radicand.</param>
-		/// <param name="num2">Degree.</param>
+		/// <param name="num1">Radicand. num1 in (0,inf)</param>
+		/// <param name="num2">Degree. num2 in (0,inf)</param>
 		/// <returns>The root.</returns>
 		double sqrt(double num1, double num2);
 
 		/// <summary>
 		/// Logarithm with general base.
 		/// </summary>
-		/// <param name="num1">Base.</param>
-		/// <param name="num2">Antilogarthm.</param>
+		/// <param name="num1">Base. num1 in (0,inf)-{1}</param>
+		/// <param name="num2">Antilogarthm. num2 in (0,inf)</param>
 		/// <returns>The logarithm.</returns>
 		double log(double num1, double num2);
 	}
