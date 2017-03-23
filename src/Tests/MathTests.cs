ff377bb3a1c3f9d877e9a008a32b689dcae08ef6
Dominik Harmim <xharmi00@stud.fit.vutbr.cz>
2017-03-23 12:25:36 +0100

    #8 Create tests for math library by Interface IMath
    - inicialization

---
+++ src/Tests/MathTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+	[TestClass]
+	public class MathTests
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+		}
+	}
+}
---
+++ src/Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bc618e0f-d983-41a6-9446-d8553670673c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
