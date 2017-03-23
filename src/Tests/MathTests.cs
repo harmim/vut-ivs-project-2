3205d20d74d297d4d863788c3349bcb1940bdff6
Dominik Harmim <xharmi00@stud.fit.vutbr.cz>
2017-03-23 12:41:40 +0100

    #8 Create tests for math library by Interface IMath
    - Added header with info about authors.

--- src/Tests/MathTests.cs
+++ src/Tests/MathTests.cs
@@ -1,6 +1,15 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿/**************************************************************
+ * Team:      Disassembler
+ *
+ * Authors:   Vojtěch Hertl <xhertl04@stud.fit.vutbr.cz>
+ *            Dominik Harmim <xharmi00@stud.fit.vutbr.cz>
+ *            Timotej Halás <xhalas10@stud.fit.vutbr.cz>
+ *            Matej Havlas <xhavla06@stud.fit.vutbr.cz>
+ **************************************************************/
 
-namespace Tests
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Disassembler.Tests
 {
 	[TestClass]
 	public class MathTests
