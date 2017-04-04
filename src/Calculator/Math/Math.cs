f5fc7a1c92c79ee512707c441bc64939ed1d8244
Matej Havlas <xhavla06@stud.fit.vutbr.cz[alias]>
2017-04-04 15:10:26 +0200

    #7 Implementation of math library by interface IMath and Tests

--- src/Calculator/Math/Math.cs
+++ src/Calculator/Math/Math.cs
@@ -15,30 +15,69 @@ namespace Disassembler.Calculator.Math
 	{
 		public double Sum(double num1, double num2)
 		{
-			throw new NotImplementedException();
+            double result = 0;
+            result = num1 + num2;
+			return result;
 		}
 
 		public double Sub(double minuend, double subtrahend)
 		{
+            double result = 0;
+            result = minuend - subtrahend;
+            return result;
 			throw new NotImplementedException();
 		}
 
 		public double Div(double divident, double divisor)
 		{
-			throw new NotImplementedException();
+            double result = 0;
+            if ( divisor == 0)
+            {
+                throw new DivideByZeroException();
+            } 
+            else
+            {
+                result = divident / divisor;
+            }
+            return result;
+			
 		}
 
 		public double Mult(double num1, double num2)
 		{
-			throw new NotImplementedException();
+            double result = 0;
+            result = num1 * num2;
+            return result;
 		}
 
-		public ulong Fact(double num)
-		{
-			throw new NotImplementedException();
-		}
+        public double Fact(double num)
+        {
+            double result = num;
 
-		public double Pow(double basis, double exponent)
+            if (num <0 )
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            else if (num == 0)
+            {
+                result = 1;
+            }
+            else if ((num > 0) && (num % 1 == 0))
+            {
+                
+                for (int i = 1; i < num; i++)
+                {
+                    result = result * (num - i);
+                }
+            }
+            
+
+            
+            return result;
+        } 
+
+        public double Pow(double basis, double exponent)
 		{
 			double result = System.Math.Pow(basis, exponent);
 
